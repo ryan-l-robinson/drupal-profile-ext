073d89cc15e0d5c561234b980dd46b5f90e00e0e
Ryan Robinson <80788808+ryan-l-robinson@users.noreply.github.com>
2026-01-19 16:11:32 -0500

    Moved title hook to new OOP implementation

---
+++ src/Hook/ProfileTypeHooks.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\profile_ext\Hook;
+
+use Drupal\Core\Hook\Attribute\Hook;
+use Drupal\profile_ext\Entity\Profile;
+
+/**
+ * Hook implementations for changing the profile class.
+ */
+class ProfileTypeHooks {
+
+  /**
+   * Adjust user profile links.
+   */
+  #[Hook('entity_type_alter')]
+
+  protected function changeProfileType(array $entity_types): void {
+    if (isset($entity_types['profile'])) {
+      $definition = $entity_types['profile'];
+      $definition->setClass(Profile::class);
+    }
+  }
+
+}
